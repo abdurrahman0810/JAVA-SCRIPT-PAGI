ee348219b4dd435f25edba049c9a54627a94cade
abdurrahman0810 <abdurrahman0810@student.abudzar.sch.id>
2026-02-04 13:26:26 +0700

    Update

--- js-1/script.js
+++ js-1/script.js
@@ -1,15 +1,12 @@
-let name = "John Doe";
+let name = "John Doe ";
 
 let age = 25;
 
 let isMarried = true;
 
-console.log("--- Hasil Variabel ---");
-console.log("Nilai name: " + name);
-console.log("Nilai age: " + age);
-console.log("Nilai isMarried: " + isMarried);
+console.log( name + age, isMarried);
 
-console.log("\n--- Pengecekan Tipe Data ---");
-console.log("Tipe data name adalah: " + typeof name);
-console.log("Tipe data age adalah: " + typeof age);
-console.log("Tipe data isMarried adalah: " + typeof isMarried); 
+
+
+
+
