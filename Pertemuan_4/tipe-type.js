1a94201c1f41dfd0f4aba0154d950dbdac7a2717
abdurrahman0810 <abdurrahman0810@student.abudzar.sch.id>
2026-02-04 11:24:28 +0700

    JS - ONE

---
+++ Pertemuan_4/tipe-type.js
@@ -0,0 +1,86 @@
+let room = 80;
+let home = 'Bogor'
+
+
+
+// Update value Data type
+
+let box = 'Snack'
+box = 50;
+console.log(box)
+
+let number = 50;
+let float = 20.5;
+let bignumber = Infinity; 
+
+console.log(number)
+console.log(float)
+
+
+
+console.log(1/0);
+console.log(bignumber);
+
+
+
+
+
+
+
+
+
+//!   Big int 
+
+
+
+let bignum = 2578754375876382752564585643573n;
+console.log(bignum)
+
+
+
+
+// !  String
+
+let adress = "bogor"
+// let name = 'midut'   
+
+let firstname = "badut"
+
+
+let lastname = `rafka ${adress}`;
+
+console.log (lastname)
+
+
+let fullname = `${firstname} ${lastname}  $ {1 + 2}`;
+console.log(fullname)
+
+
+
+
+
+
+// ! boleean (Logika)
+
+
+
+let isMarried = false;
+let isAgree = true;
+
+
+console.log(isMarried)
+console.log(isAgree)
+
+
+
+
+
+
+let isTrue =  1;
+let isFalse = 2;
+
+console.log(isTrue)
+console.log(isFalse)
+
+
+console.log (14 > 1)
---
+++ js-1/script.js
@@ -0,0 +1,15 @@
+let name = "John Doe";
+
+let age = 25;
+
+let isMarried = true;
+
+console.log("--- Hasil Variabel ---");
+console.log("Nilai name: " + name);
+console.log("Nilai age: " + age);
+console.log("Nilai isMarried: " + isMarried);
+
+console.log("\n--- Pengecekan Tipe Data ---");
+console.log("Tipe data name adalah: " + typeof name);
+console.log("Tipe data age adalah: " + typeof age);
+console.log("Tipe data isMarried adalah: " + typeof isMarried); 
